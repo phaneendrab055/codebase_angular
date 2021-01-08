0723e8bdf4a7546aca7e4068f335899fd154f032
Phaneendra Bheesetti <phaneendra.bheesetti@claimtherapist.com>
2021-01-08 15:42:18 +0530

    Commit2

--- countdown-rxjs/src/app/directives/input-to-countdown.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { InputToCountdownDirective } from './input-to-countdown.directive';
-
-describe('InputToCountdownDirective', () => {
-  it('should create an instance', () => {
-    const directive = new InputToCountdownDirective();
-    expect(directive).toBeTruthy();
-  });
-});
